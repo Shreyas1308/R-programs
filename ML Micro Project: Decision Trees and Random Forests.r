83ebf049f147c96184cc99d792c41fb7fd9ea965
Shreyas1308 <56815931+Shreyas1308@users.noreply.github.com>
2021-08-24 20:21:06 +0530

    Update ML Micro Project: Decision Trees and Random Forests.r

--- ML Micro Project: Decision Trees and Random Forests.r
+++ ML Micro Project: Decision Trees and Random Forests.r
@@ -12,3 +12,7 @@ ggplot(df,aes(Room.Board,Grad.Rate)) + geom_point(aes(color=Private))
 ggplot(df,aes(F.Undergrad)) + geom_histogram(aes(fill=Private),color="black",bins=50)
 
 ggplot(df,aes(Grad.Rate)) + geom_histogram(aes(fill=Private),color="black",bins=50)
+
+subset(df,Grad.Rate>100)
+
+df['Cazenovia College','Grad.Rate'] <- 100
